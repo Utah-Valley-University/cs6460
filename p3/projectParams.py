40943dc741d71e1d492993e01f5fad293d49a0eb
GRudolph <george.rudolph@uvu.edu>
2024-11-24 12:03:05 -0700

    Update projectParams.py
    - Added comprehensive module docstring
    - Added variables section to docstring
    - Verified Python 3.13 compatibility
    - Added modifier attribution

--- p3/projectParams.py
+++ p3/projectParams.py
@@ -1,5 +1,26 @@
-# projectParams.py
-# ----------------
+"""
+Project parameters for Pacman reinforcement learning.
+
+This module defines configuration parameters for Project 3 which implements
+reinforcement learning algorithms in the Pacman environment. It specifies
+the student code files to be submitted, test classes, and project metadata.
+
+Variables:
+    STUDENT_CODE_DEFAULT: List of required student implementation files
+    PROJECT_TEST_CLASSES: Test class module for autograding
+    PROJECT_NAME: Name and number of this project
+    BONUS_PIC: Flag for enabling bonus visualization
+
+Python Version: 3.13
+Last Modified: 24 Nov 2024
+Modified by: George Rudolph
+
+Changes:
+- Added comprehensive module docstring
+- Added variables section to docstring
+- Verified Python 3.13 compatibility
+- Added modifier attribution
+
 # Licensing Information:  You are free to use or extend these projects for
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
@@ -10,7 +31,7 @@
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
 # Student side autograding was added by Brad Miller, Nick Hay, and
 # Pieter Abbeel (pabbeel@cs.berkeley.edu).
-
+"""
 
 STUDENT_CODE_DEFAULT = 'analysis.py,qlearningAgents.py,valueIterationAgents.py'
 PROJECT_TEST_CLASSES = 'reinforcementTestClasses.py'
