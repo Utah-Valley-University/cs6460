a2b43e4efa437d463ee403d8235a5d2538e68c7a
GRudolph <george.rudolph@uvu.edu>
2024-11-24 16:07:18 -0700

    Update autograder.py
    fixed invalid escape sequence in regex expressions. now raw strings.

--- p3/autograder.py
+++ p3/autograder.py
@@ -390,8 +390,8 @@ def evaluate(generateSolutions: bool, testRoot: str, moduleDict: dict, exception
 
         # load test cases into question
         tests = [t for t in os.listdir(
-            subdir_path) if re.match('[^#~.].*\.test\Z', t)]
-        tests = [re.match('(.*)\.test\Z', t).group(1) for t in tests]
+            subdir_path) if re.match(r'[^#~.].*\.test\Z', t)]
+        tests = [re.match(r'(.*)\.test\Z', t).group(1) for t in tests]
         for t in sorted(tests):
             test_file = os.path.join(subdir_path, f'{t}.test')
             solution_file = os.path.join(subdir_path, f'{t}.solution')
@@ -472,10 +472,10 @@ if __name__ == '__main__':
 
     moduleDict = {}
     for cp in codePaths:
-        moduleName = re.match('.*?([^/]*)\.py', cp).group(1)
+        moduleName = re.match(r'.*?([^/]*)\.py', cp).group(1)
         moduleDict[moduleName] = loadModuleFile(
             moduleName, os.path.join(options.codeRoot, cp))
-    moduleName = re.match('.*?([^/]*)\.py', options.testCaseCode).group(1)
+    moduleName = re.match(r'.*?([^/]*)\.py', options.testCaseCode).group(1)
     moduleDict['projectTestClasses'] = loadModuleFile(
         moduleName, os.path.join(options.codeRoot, options.testCaseCode))
 
